2d31a6b2119f19e987087cd7f27b608c257c3c12
douhaoYaz <FusionYaz@163.com>
2021-12-05 17:22:25 +0800

    DAO层增加了注册功能需要的数据库查询和插入

--- UserLogin/src/pers/douhaoyaz/userlogin/DAO/DAO.java
+++ UserLogin/src/pers/douhaoyaz/userlogin/DAO/DAO.java
@@ -51,9 +51,43 @@ public class DAO {
 
     }
 
-    public static User register(String uname, String upwd){
+    public static int register(String uname, String upwd){
 
-        User user;
-        return null;
+        // 调用DbUtil的getMysqlConn方法与MySQL数据库建立连接
+        Connection connection = DbUtil.getMysqlConn();
+        // 判断是否连接成功
+        if(connection != null){
+            try {
+                // 获取执行sql对象statment
+                Statement st = connection.createStatement();
+                // 定义SQL语句
+                String sqlString = "SELECT userName FROM user";
+                // 使用resultSet接收返回数据
+                ResultSet resultSet = st.executeQuery(sqlString);
+                // 根据uname账号名在数据库查找对应账号
+                while (resultSet.next()){
+                    // 若查找到，说明该账号名已被使用，注册失败
+                    if(uname.equals(resultSet.getString("userName")))
+                        return 2;   // 2表示账号名已被使用
+                }
+
+                // 在user数据表中要插入新注册的用户信息
+                sqlString = String.format("INSERT INTO user (userName, userPwd) values('%s', '%s')", uname, upwd);
+                // 执行插入操作
+                st.executeUpdate(sqlString);
+
+                // 释放资源
+                st.close();
+                connection.close();
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+                return 0;   // 0表示数据库操作失败
+            }
+        }
+
+        return 1;   // 1表示注册成功
     }
+
+
 }
